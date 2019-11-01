c6133d530526f78c744dc5b391e3b9302ff02dc8
Emily Biegas <emilybiegas@gmail.com>
2019-11-01 10:55:38 -0400

    working copy

--- script.js
+++ script.js
@@ -1,35 +1,42 @@
 "use strict";
-
 const platformEl = document.getElementById("platform");
 
-let i = 0;
-for (i = 0; i < 9; i++) {
+const resetEl = document.createElement("div");
+resetEl.classList.add("reset");
+resetEl.innerText = "Reset";
+
+for (let i = 0; i < 8; i++) {
+  const containerEl = document.createElement("div");
+  containerEl.classList.add("card-container");
   const cardEl = document.createElement("div");
   cardEl.classList.add("card");
-  platformEl.appendChild(cardEl);
-  const containerEl = document.createElement("div");
-  cardEl.appendChild(containerEl);
   const picFrontEl = document.createElement("div");
   picFrontEl.classList.add("genericPic");
   const picBackEl = document.createElement("div");
-
   picBackEl.classList.add("backPic");
-  containerEl.appendChild(picFrontEl);
-  containerEl.appendChild(picBackEl);
+  cardEl.appendChild(picFrontEl);
+  cardEl.appendChild(picBackEl);
+  containerEl.appendChild(cardEl);
+  platformEl.appendChild(containerEl);
 }
 
-platformEl.addEventListener("click", colorChange);
+// const cards = document.querySelectorAll('.card');
 
-function colorChange(e) {
-  if (e.target.classList.contains("card")) {
-  }
-  e.target.style.backgroundColor = "red";
-}
+// cards.forEach(card => card.addEventListener('click', flipCard()));
 
+// resetEl.addEventListener('click', shuffle());
+
+// platformEl.addEventListener("click", startTimer());
+
+// function colorChange(e) {
+//   if (e.target.classList.contains("card")) {
+//   }
+//   e.target.style.backgroundColor = "red";
+// }
 
 // >>>>>>> Stashed changes
 
-// hints from Mitch: 
+// hints from Mitch:
 
 // let checkArray = []
 // if(checkArray[0].backgroundImage)
@@ -50,11 +57,3 @@ function colorChange(e) {
 //     }
 //     return array;
 //   }
-
-/* what this lab is doing: 
-1. function that flips two cards at a time
-    - event listener on the container that flips card on click
-2. checks to see if those cards match by checking some property like background image
-3. if they match, they disappear from page (change class to stgh with display: none)
-4. if they do not match, flips the cards back 
-
