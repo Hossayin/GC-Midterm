c6b2026e7147247dd0180382fab8da7a011f0f28
unknown <dequanchambers94@gmail.com>
2019-11-01 16:59:41 -0400

    we need some ducktape

--- script.js
+++ script.js
@@ -4,98 +4,71 @@ const mainEl = document.getElementById("fullPlatform");
 const cards = document.querySelectorAll(".card");
 const resetEl = document.querySelector(".reset");
 let photoArray = [
-  "lemon.png",
-  "lemon.png",
-  "lime.png",
-  "lime.png",
-  "pineapple.png",
-  "pineapple.png",
-  "watermelon.png",
-  "watermelon.png"
+    "lemon.png",
+    "lemon.png",
+    "lime.png",
+    "lime.png",
+    "pineapple.png",
+    "pineapple.png",
+    "watermelon.png",
+    "watermelon.png"
 ];
 let shuffled = shuffle(photoArray);
 
 for (let i = 0; i < 8; i++) {
-  const containerEl = document.createElement("div");
-  containerEl.classList.add("card-container");
-  const cardEl = document.createElement("div");
-  cardEl.classList.add("card");
-  const picFrontEl = document.createElement("div");
-  picFrontEl.classList.add("genericPic");
-  const picBackEl = document.createElement("div");
-  picBackEl.classList.add("backPic");
-  cardEl.appendChild(picFrontEl);
-  cardEl.appendChild(picBackEl);
-  containerEl.appendChild(cardEl);
-  platformEl.appendChild(containerEl);
-  picBackEl.style.backgroundImage = `url(./images/${shuffled[i]})`;
-
-  // switch (i) {
-  //   case 0:
-  //     cardEl.id = "a";
-  //     break;
-  //   case 1:
-  //     cardEl.id = "b";
-  //     break;
-  //   case 2:
-  //     cardEl.id = "c";
-  //     break;
-  //   case 3:
-  //     cardEl.id = "d";
-  //     break;
-  //   case 4:
-  //     cardEl.id = "e";
-  //     break;
-  //   case 5:
-  //     cardEl.id = "f";
-  //     break;
-  //   case 6:
-  //     cardEl.id = "g";
-  //     break;
-  //   case 7:
-  //     cardEl.id = "h";
-  //     break;
-  // }
+    const containerEl = document.createElement("div");
+    containerEl.classList.add("card-container");
+    const cardEl = document.createElement("div");
+    cardEl.classList.add("card", "flipper");
+    const picFrontEl = document.createElement("div");
+    picFrontEl.classList.add("genericPic");
+    const picBackEl = document.createElement("div");
+    picBackEl.classList.add("backPic");
+    cardEl.appendChild(picFrontEl);
+    cardEl.appendChild(picBackEl);
+    containerEl.appendChild(cardEl);
+    platformEl.appendChild(containerEl);
+    picBackEl.style.backgroundImage = `url(./images/${shuffled[i]})`;
+    cardEl.addEventListener("click", flipCard);
 }
 
 resetEl.addEventListener("click", shuffle);
 
-// function shuffle(e) {
-//   ifcards.forEach(card => {
-//     let randomPos = Math.floor(Math.random() * 8);
-//     card.style.order = randomPos;
-//   });
-// }
-
 function shuffle(array) {
-  var currentIndex = array.length,
-    temporaryValue,
-    randomIndex;
+    var currentIndex = array.length,
+        temporaryValue,
+        randomIndex;
 
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
 
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
 
-  return array;
+    return array;
 }
-
-cards.forEach(card => card.addEventListener("click", flipCard));
 
 platformEl.addEventListener("click", countdown);
 
 // Shows back face of card when you click
-function flipCard() {}
+// cardEl.addEventListener("click", flipCard);
+// cardEl.className.add("flipper");
+
+function flipCard(e) {
+    console.log(e.target);
+    if (e.target.classList.contains("genericPic")) {
+        e.target.className.toggle("backPic");
+    }
+}
 
 function checkMatch() {
-  function disappear() {}
+    function disappear() {}
 }
 
 // Prevents user from clicking a third card
@@ -108,35 +81,35 @@ function unflipCards() {}
 function resetBoard() {}
 
 function countdown() {
-  let counter = document.getElementById("counter");
-  let seconds = 60;
-  const timer = setInterval(function() {
-    if (seconds > 0) {
-      seconds--;
-      counter.innerText = `00:${seconds}`;
-    } else if (seconds === 0) {
-      clearInterval(timer);
-      // then remove event listener for flipping
-    }
-  }, 1000);
-  // if (event.target.classList.contains("card-container")) {
-  // let seconds = 60;
-  // let mins = 1;
-  // let counter = document.getElementById("counter");
-  // let currentMinutes = mins - 1;
-  // seconds--;
+    let counter = document.getElementById("counter");
+    let seconds = 60;
+    const timer = setInterval(function() {
+        if (seconds > 0) {
+            seconds--;
+            counter.innerText = `00:${seconds}`;
+        } else if (seconds === 0) {
+            clearInterval(timer);
+            // then remove event listener for flipping
+        }
+    }, 1000);
+    // if (event.target.classList.contains("card-container")) {
+    // let seconds = 60;
+    // let mins = 1;
+    // let counter = document.getElementById("counter");
+    // let currentMinutes = mins - 1;
+    // seconds--;
 
-  // counter.innerHTML =
-  //   currentMinutes.toString() +
-  //   ":" +
-  //   (seconds < 10 ? "0" : "") +
-  //   String(seconds);
-  // if (seconds > 0) {
-  //   setInterval(function test() {
-  //     seconds--;
-  //   }, 1000);
-  // } else {
-  //   countdown(mins - 1);
-  // }
-  // }
+    // counter.innerHTML =
+    //   currentMinutes.toString() +
+    //   ":" +
+    //   (seconds < 10 ? "0" : "") +
+    //   String(seconds);
+    // if (seconds > 0) {
+    //   setInterval(function test() {
+    //     seconds--;
+    //   }, 1000);
+    // } else {
+    //   countdown(mins - 1);
+    // }
+    // }
 }
